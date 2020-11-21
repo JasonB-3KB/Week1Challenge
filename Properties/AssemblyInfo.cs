19f7963c7bb404f89ff1a5a8b588789b87ff2ebb
Jason Bagwill <mr.bagwill@gmail.com>
2020-11-21 14:39:11 -0500

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week1ThursdayChallenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week1ThursdayChallenge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ef712bf3-f9c5-41d4-a7eb-2a0ba19370f5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week1Thursday.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Week1ThursdayChallenge
+{
+    [TestClass]
+    public class Week1Thursday
+    {
+        
+        [TestMethod]
+        public void SuperCaliFrag()
+        {
+            string name = "Supercalifragilisticeexpialidocious";
+
+            //foreach //3  //2  //4  //1
+            foreach (char letter in name)
+            {
+                if (letter == 'i')
+                {
+                    Console.WriteLine(letter);
+                } 
+                else
+                {
+                    Console.WriteLine("Not an I");
+                }
+                
+                
+            }
+            
+        }
+        [TestMethod]
+        public void ArrayChallenge()
+        {
+            string name = "Favorite Movies:";
+            Console.WriteLine(name);
+            string[] favoriteMovies = { "Back to the Future ", "Back to the Future 2 ", "Saving Private Ryan" };
+            foreach (string i in favoriteMovies)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+        [TestMethod]
+        public void NameAndAge()
+        {
+            string firstName = "Jason";
+            string lastName = "Bagwill";
+            int age = 45;
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+        }
+
+        [TestMethod]
+        public void Dates()
+        {
+            DateTime today = DateTime.Now;
+            DateTime newDate2 = today.AddDays(30);
+            DateTime newDate3 = today.AddDays(60);
+            Console.WriteLine(today);
+            Console.WriteLine(newDate2);
+            Console.WriteLine(newDate3);
+        }
+
+        [TestMethod]
+        public void AgePlusSeven()
+        {
+            int numOne = 45;
+            int numTwo = 7;
+
+            int sum = numOne + numTwo;
+            Console.WriteLine(sum);
+            int diff = numOne - numTwo;
+            Console.WriteLine(diff);
+            int prod = numOne * numTwo;
+            Console.WriteLine(prod);
+            int quot = numOne / numTwo;
+            Console.WriteLine(quot);
+            int remainder = numOne % numTwo;
+            Console.WriteLine(remainder);
+        }
+
+        [TestMethod]
+        public void HoursOfSleep()
+        {
+            int hours = 4;
+
+            if (hours > 10)
+            {
+                Console.WriteLine("Wow! That's a lot of sleep!");
+            }
+            else if (hours > 8)
+            {
+                Console.WriteLine("You should be pretty rested!");
+            }
+            else if (hours > 4)
+            {
+                Console.WriteLine("Bummer");
+            }
+            else if (hours > 0)
+            {
+                Console.WriteLine("OH MAN! GET SOME SLEEP");
+            }
+        }
+        [TestMethod]
+        public void Switcheroo()
+        {
+            int mood = 4;
+            switch (mood)
+            {
+                case 10:
+                    Console.WriteLine("Great");
+                    break;
+                case 9:
+                    Console.WriteLine("Good");
+                    break;
+                case 8:
+                    Console.WriteLine("Okay");
+                    break;
+                case 7:
+                    Console.WriteLine("Bad");
+                    break;
+                default:
+                    Console.WriteLine(":( Hopefully improving");
+                    break;
+
+            }
+         }
+    }
+}
+
